83d2dac151f4fefe4618b4a0e0b6cc8684dd0af5
Valentine BM <vbmueller@bmgww.com>
2018-07-02 17:54:21 +0800

    authenticate to create post or delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "valentine", password: "valentine", only: [:destroy]
+
   def create
     @post= Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
