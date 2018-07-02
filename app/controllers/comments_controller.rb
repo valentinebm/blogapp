6d00ace7ae93973a61284e7be2e0af9bafb328a6
Valentine BM <vbmueller@bmgww.com>
2018-07-02 18:05:16 +0800

    devise installed user model generated relationship with blog post added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "valentine", password: "valentine", only: [:destroy]
+  # http_basic_authenticate_with name: "valentine", password: "valentine", only: [:destroy]
 
   def create
     @post= Post.find(params[:post_id])
